a57898d97cf8e26ceb15ee440af5daecded61f80
Elie Cohen <ec92009@gmail.com>
2022-10-18 11:40:43 +0100

    basic if else (like C)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+
+    let b: bool = 1f32 < 3.0;
+    println!("Hello, world!, {}", b);
+
+    let food = "cookie";
+    if food == "cookie" {
+        println!("yes cookie");
+    } else {
+        println!("yes NOT cookie");
+
+    }
+
+    let food = "NOT cookie";
+    if food == "cookie" {
+        println!("yes cookie");
+    } else {
+        println!("too bad NOT cookie");
+
+    }
+
 }
